b17d76eb093d64bf8b38b076379fe5ea1942df10
harshalthakre123 <harshalthakre.csaiml.oist@gmail.com>
2025-01-25 23:50:06 +0530

    added new files(json)

---
+++ json/renter.js
@@ -0,0 +1,14 @@
+// Heading Animation ======================================================
+new TypeIt("#heading", {
+    strings: " Car Renter's Details.",
+    speed: 75,
+    loop: true,
+  }).go();
+
+
+// JSON=====================================================================
+
+let getdata= async ()=>{
+  
+}
+
